407bfea9bb7bc7f9426d8130367c6754bd1a8c34
yukorisnik <starcooler2@yahoo.se>
2025-11-26 14:33:58 +0100

    Update Calculator.java

--- src/main/java/systementor/cidemo1/Calculator.java
+++ src/main/java/systementor/cidemo1/Calculator.java
@@ -3,21 +3,21 @@ package systementor.cidemo1;
 //public class Calculator {
 
     public int add(int a, int b) {
-        return a + b + 4;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
-        return 10;
+        return -1;
     }
 
     public int multiply(int a, int b) {
-        return a * b * 100;
+        return a * b;
     }
 
     public int divide (int a, int b) {
         if (b == 0){
             throw new IllegalArgumentException("Divide by zero");
         }
-        return a / 3;
+        return a / b;
     }
 }
